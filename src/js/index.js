5abcdcbd8274891b20dbffa243d35b776cecad26
M.Mykhalyuk <mim@qapint.com>
2016-06-09 23:20:10 +0300

    add redux

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Slider from './slider.js';
+import {createStore} from 'redux';
 
-ReactDOM.render(<Slider />, document.getElementById('app'));
+const mover = (state = { value: 10 }, action) => {
+	switch (action.type) {
+		case 'MOVE_RIGTH' :
+			return {
+				value: state.value + 1
+			}
+		case 'MOVE_LEFT' :
+			return {
+				value: state.value - 1
+			}
+		default:
+			return state;
+	}
+}
+
+const store = createStore(mover);
+
+const Slider = ({
+	value,
+	moveRigth,
+	moveLeft
+}) => (
+	<div>
+		<button onClick={moveLeft}>move left</button>
+		<input min="1" max="100" type="range" value={value} />
+		<button onClick={moveRigth}>move rigth</button>
+		<h2>{value}</h2>
+	</div>
+)
+
+const render = () => {
+	ReactDOM.render(
+		<Slider
+			value={store.getState().value} 
+			moveRigth={() => 
+				store.dispatch({
+					type: 'MOVE_RIGTH'
+				})
+			} 
+			moveLeft={() => 
+				store.dispatch({
+					type: 'MOVE_LEFT'
+				})
+			} 
+		/>,
+		 document.getElementById('app'))
+}
+
+store.subscribe(render);
+
+render();
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,5 +1,24 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
+
+const {createStore} = require('redux');
+
+const counter = (state = { value: 10}, action) => {
+	switch (action.type) {
+		case 'MOVE_RIGTH' :
+			return {
+				value: state.value + 1
+			}
+		case 'MOVE_LEFT' :
+			return {
+				value: state.value - 1
+			}
+		default:
+			return state;
+	}
+}
+
+const store = createStore(counter);
 
 export default class Slider extends React.Component {
 	constructor(){
@@ -18,12 +37,20 @@ export default class Slider extends React.Component {
 		})
 	}
 
+	moveRigth(){
+		store.dispatch({
+			type: 'MOVE_RIGTH'
+		})
+	}
+
 	render(){
 		return (
 			<div>
 				<input type="range" defaultValue={this.state.value} onChange={this.update}/>
 				<p>{this.state.value}</p>
+				<button onClick={this.moveRigth}>move rigth</button>
 			</div>
 		)
 	}
 }
+*/
