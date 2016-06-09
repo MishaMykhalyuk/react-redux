ff4f0d2d78551f697119012452249603cd972fb3
M.Mykhalyuk <mim@qapint.com>
2016-06-09 21:14:22 +0300

    add slider component

--- src/js/slider.js
+++ src/js/slider.js
@@ -4,9 +4,26 @@ import ReactDOM from 'react-dom';
 export default class Slider extends React.Component {
 	constructor(){
 		super();
+
+		this.state = {
+			value: 20
+		}
+
+		this.update = this.update.bind(this);
+	}
+
+	update(event){
+		this.setState({
+			value: event.target.value
+		})
 	}
 
 	render(){
-		return <p>hello world</p>
+		return (
+			<div>
+				<input type="range" defaultValue={this.state.value} onChange={this.update}/>
+				<p>{this.state.value}</p>
+			</div>
+		)
 	}
 }
