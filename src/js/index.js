f7e4b28a9f2d236ca56a906e5802bd75ac697b90
M.Mykhalyuk <mim@qapint.com>
2016-07-14 23:56:04 +0300

    implement redux using rx

--- src/js/index.js
+++ src/js/index.js
@@ -1,55 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import Rx from "rxjs";
+import createState from './createState.js'
+import {Observable} from "rxjs/Observable";
 
-const mover = (state = { value: 10 }, action) => {
-	switch (action.type) {
-		case 'MOVE_RIGTH' :
-			return {
-				value: state.value + 1
-			}
-		case 'MOVE_LEFT' :
-			return {
-				value: state.value - 1
-			}
-		default:
-			return state;
+function createReducer(...observables) {
+	return Observable.merge.apply(Observable, observables);
+};
+
+const moverActions = {
+	moveRigth$: new Rx.Subject,
+	moveLeft$: new Rx.Subject
+};
+
+const moverReducer = createReducer(
+	moverActions.moveRigth$.map(value => 
+		state => Object.assign(state, { value: state.value + value })
+	),
+
+	moverActions.moveLeft$.map(value => 
+		state => Object.assign(state, { value: state.value - value })
+	)
+);
+
+const reducer = createReducer(moverReducer);
+
+const store = createState(reducer, Rx.Observable.of({ value: 10 }));
+
+const Slider = React.createClass({
+	propTypes: {
+		value: React.PropTypes.element.isRequired,
+		moveRigth: React.PropTypes.func.isRequired,
+		moveLeft: React.PropTypes.func.isRequired
+	},
+
+	render: function() {
+		return (
+			<div>
+				<button onClick={this.props.moveLeft}>move left</button>
+				<input min="1" max="100" type="range" value={this.props.value} />
+				<button onClick={this.props.moveRigth}>move rigth</button>
+				<h2>{this.props.value}</h2>
+			</div>
+		);
 	}
-}
+});
 
-const store = createStore(mover);
-
-const Slider = ({
-	value,
-	moveRigth,
-	moveLeft
-}) => (
-	<div>
-		<button onClick={moveLeft}>move left</button>
-		<input min="1" max="100" type="range" value={value} />
-		<button onClick={moveRigth}>move rigth</button>
-		<h2>{value}</h2>
-	</div>
-)
-
-const render = () => {
+const render = state => {
 	ReactDOM.render(
 		<Slider
-			value={store.getState().value} 
-			moveRigth={() => 
-				store.dispatch({
-					type: 'MOVE_RIGTH'
-				})
-			} 
-			moveLeft={() => 
-				store.dispatch({
-					type: 'MOVE_LEFT'
-				})
-			} 
+			value={state.value} 
+			moveRigth={ () => moverActions.moveRigth$.next(1) }
+			moveLeft={ () => moverActions.moveLeft$.next(1) }
 		/>,
-		 document.getElementById('app'))
+		 document.getElementById('app')
+	)
 }
 
 store.subscribe(render);
-
-render();
